d920031b97b89735739c3954ff9db4f7529c6e2e
Adam Porter <adam@alphapapa.net>
2020-02-24 22:47:44 -0600

    This is pretty cool

--- buffer-groups.el
+++ buffer-groups.el
@@ -37,14 +37,29 @@
 
 (defvar buffer-groups-groups nil)
 
+(defvar buffer-groups-current-group nil)
+
 ;;;; Grouping macro
 
 (group-tree-defmacro buffer-groups-defgroups
   `((dir (&rest dirs) `(group-by 'buffer-groups-group-dir ',dirs))
-    (mode (name &rest modes) `(group-by 'buffer-groups-group-mode ',modes ,name))))
+    (mode (name &rest modes) `(group-by 'buffer-groups-group-mode ',modes ,name))
+    (mode-match (name &rest regexps) `(group-by 'buffer-groups-group-mode-match ',regexps ,name))
+    (name-match (name &rest regexps) `(group-by 'buffer-groups-group-name-match ',regexps ,name))
+    (auto-project () `(group-by 'buffer-groups-group-auto-project))))
 
 ;;;; Customization
 
+(defgroup buffer-groups nil
+  "FIXME"
+  :group 'convenience)
+
+(defcustom buffer-groups-filter-fns
+  (list (lambda (buffer)
+          "Return non-nil if BUFFER's name starts with a space."
+          (string-prefix-p " " (buffer-name buffer))))
+  "FIXME"
+  :type '(repeat function))
 
 ;;;; Commands
 
@@ -52,6 +67,28 @@
   "Apply `buffer-groups' grouping to buffers."
   (interactive)
   (group-tree buffer-groups-groups (buffer-list) ))
+
+(defun buffer-groups-grouped ()
+  "Return buffers grouped."
+  (group-tree buffer-groups-groups
+              (cl-loop with buffers = (buffer-list)
+                       for fn in buffer-groups-filter-fns
+                       do (setf buffers (cl-remove-if fn buffers))
+                       finally return buffers)))
+
+(defun buffer-groups-switch-group ()
+  (interactive)
+  (when-let* ((selected-group
+               (completing-read "Group: " (mapcar #'car (buffer-groups-grouped)))))
+    (setf buffer-groups-current-group selected-group)))
+
+(defun buffer-groups-switch-buffer ()
+  (interactive)
+  (let* ((group (completing-read "Group: " (mapcar #'car (buffer-groups-grouped))))
+         (buffer (completing-read "Buffer: "
+                                  (mapcar #'buffer-name
+                                          (alist-get group (buffer-groups-grouped) nil nil #'string=)))))
+    (switch-to-buffer buffer)))
 
 ;;;; Functions
 
@@ -66,6 +103,22 @@
   (when (member (buffer-local-value 'major-mode buffer) modes)
     (or name (car modes))))
 
+(defun buffer-groups-group-mode-match (regexps name buffer)
+  (cl-loop with mode-name = (symbol-name (buffer-local-value 'major-mode buffer))
+           for regexp in regexps
+           when (string-match-p regexp mode-name)
+           return (or name mode-name)))
+
+(defun buffer-groups-group-name-match (regexps name buffer)
+  (cl-loop for regexp in regexps
+           when (string-match-p regexp (buffer-name buffer))
+           return (or name regexp)))
+
+(defun buffer-groups-group-auto-project (buffer)
+  (when-let* ((project (with-current-buffer buffer
+                         (project-current)))
+              (project-root (car (project-roots project))))
+    (concat "Project: " project-root)))
 
 ;;;; Footer
 
